bf0c10ab96e35807383dd34d31ae7b66d441c2f9
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-12-12 15:52:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
+#include <lib.h>
+using namespace std;
 int main(){
+  int d=0; cin >> d; float numero [d], float x=0;
+    for (int i=0; i<d; i++) {
+        cin >> numero[i];
+    } for (int i=1; i<d; i++) {
+        float n=funzione (numero[i], numero[i-1]);
+    if (n>x) {
+        x=n;
+    }
+    }
+    cout <<x;
   return 0;
 }
