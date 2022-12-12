07e3f5d240e6faeae37d42f0c2afb19a54214c27
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-12-12 15:53:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <lib.h>
 using namespace std;
 int main(){
-  int d=0; cin >> d; float numero [d], float x=0;
+  int d=0; cin >> d; float numero [d]; float x=0;
     for (int i=0; i<d; i++) {
         cin >> numero[i];
     } for (int i=1; i<d; i++) {
